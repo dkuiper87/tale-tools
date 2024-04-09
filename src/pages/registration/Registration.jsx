e6a25a4477e9cce4d0cbf2e4900c21c7d328114b
dkuiper87 <dkuiper87@gmail.com>
2024-04-09 13:37:10 +0200

    Formulier validatie toegevoegd.

--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -2,22 +2,49 @@ import {useForm} from "react-hook-form";
 import axios from "axios";
 
 function Register() {
-    const { register } = useForm();
+    const { register, getValues } = useForm();
 
     {/* Function to handle the form submit */}
+/*    function handleFormSubmit(data) {
 
+        {/!* Add user role to the data *!/}
+        const updatedData = {
+            ...data,
+            role: ["user"]
+        }
+
+        async function createAccount() {
+            try {
+
+            }
+        }
+
+    }*/
 
     return (
         <>
             <p>This is the Registration Page</p>
             {/* Add form here */}
             <form>
-                <label htmlFor="user-name-field">
+                <label htmlFor="username-field">
                     User Name:
                     <input
                         type="text"
-                        id="user-name-field"
-                        {...register("user-name")}
+                        id="username-field"
+                        {...register("username", {
+                            required: {
+                                value: true,
+                                message: "Please pick a username.",
+                            },
+                            minlength: {
+                                value: 6,
+                                message: "Your username needs to be at least 6 characters.",
+                            },
+                            maxlength: {
+                                value: 25,
+                                message: "Your username needs to be 25 characters or less.",
+                            },
+                        })}
                     />
                 </label>
                 <label htmlFor="email-field">
@@ -25,7 +52,16 @@ function Register() {
                     <input
                         type="text"
                         id="email-field"
-                        {...register("email")}
+                        {...register("email", {
+                            required: {
+                                value: true,
+                                message: "Please enter your E-Mail.",
+                            },
+                            pattern: {
+                                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                message: "Please enter a valid E-Mail adress.",
+                            },
+                        })}
                     />
                 </label>
                 <label htmlFor="email-confirmation-field">
@@ -33,7 +69,17 @@ function Register() {
                     <input
                         type="text"
                         id="email-confirmation-field"
-                        {...register("email-confirmation")}
+                        {...register("email-confirmation",{
+                            required: {
+                                value: true,
+                                message: "Please repeat your E-Mail.",
+                            },
+                            pattern: {
+                                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                message: "Please enter a valid E-Mail adress.",
+                            },
+                            validate: (emailMatch) => (emailMatch === getValues().email) || 'Email does not match.'
+                        })}
                     />
                 </label>
                 <label htmlFor="password-field">
@@ -41,15 +87,41 @@ function Register() {
                     <input
                         type="text"
                         id="password-field"
-                        {...register("password")}
+                        {...register("password", {
+                            required: {
+                                value: true,
+                                message: "Please choose a password.",
+                            },
+                            minlength: {
+                                value: 6,
+                                message: "Your password needs to be at least 6 characters long",
+                            },
+                            maxlength: {
+                                value: 25,
+                                message: "Your password needs to be 25 characters or less.",
+                            },
+                        })}
                     />
                 </label>
-                <label htmlFor="user-name-field">
+                <label htmlFor="password-confirmation-field">
                     Confirm Password:
                     <input
                         type="text"
                         id="password-confirmation-field"
-                        {...register("password-confirmation-name")}
+                        {...register("password-confirmation", {
+                            required: {
+                                value: true,
+                                message: "Please repeat your password.",
+                            },
+                            minlength: {
+                                value: 6,
+                                message: "Your password needs to be at least 6 characters long",
+                            },
+                            maxlength: {
+                                value: 25,
+                                message: "Your password needs to be 25 characters or less.",
+                            },
+                        })}
                     />
                 </label>
             </form>
