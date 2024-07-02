e4dbb62b11b1df1731d1fb92012be99772ab151c
dkuiper87 <dkuiper87@gmail.com>
2024-07-02 15:25:15 +0200

    DiceContext gemaakt waar de logica voor de dicerolls inzit, neemt een string aan die aangeeft welke type dice er gegooid moeten worden en hoeveel en slaat het totaal op in de context. Een interactivedicetext component gemaakt die bepaalde resultaten van de API kan doorzoeken om te zoeken naar strings voor dice rolls, en deze vervolgens vervangt voor een klikbaare versie. Tenslotte DiceRoller component gemaakt waar de gebruiker een selectie aan verschillende dice kan rollen.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 import { AuthProvider } from "./context/AuthContext.jsx";
-
+import {useState} from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import Register from "./pages/registration/Registration.jsx";
@@ -9,22 +9,27 @@ import Login from "./pages/login/Login.jsx";
 import Account from "./pages/account/Account.jsx";
 import Rules from "./pages/rules/Rules.jsx";
 import Encounters from "./pages/encounters/Encounters.jsx";
-
+import DiceRoller from "./components/diceRoller/DiceRoller.jsx";
 import Sidebar from "./components/sidebar/Sidebar.jsx";
+import {DiceProvider} from "./context/DiceContext.jsx";
 
 function App() {
+
   return (
     <>
         <AuthProvider>
-            <Sidebar/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/register" element={<Register/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/account" element={<Account/>}/>
-                <Route path="/rules" element={<Rules/>}/>
-                <Route path="/encounters" element={<Encounters/>}/>
-            </Routes>
+            <DiceProvider>
+                <Sidebar/>
+                <DiceRoller/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/account" element={<Account/>}/>
+                    <Route path="/rules" element={<Rules/>}/>
+                    <Route path="/encounters" element={<Encounters/>}/>
+                </Routes>
+            </DiceProvider>
         </AuthProvider>
     </>
   )
